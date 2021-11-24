aa69d518006946da3a8f0c0282a36eae8b12e4da
147 <lfw93814@gmail.com>
2021-11-24 22:06:43 +0800

    1

--- Test/Test_03.py
+++ Test/Test_03.py
@@ -18,3 +18,12 @@ data = dataset[0]
 data.is_undirected()
 # >>> True
 
+# 数据集包含37个节点，每个节点有3个特征。总共有168/2=84条无向边，该图仅被分配给一个class。此外，该数据对象仅有一个graph-level target
+
+# 可以使用slices, long or bool tensors来划分数据集。比如创建90/10的训练/测试划分：
+
+train_dataset = dataset[:540]
+# >>> ENZYMES(540)
+
+test_dataset = dataset[540:]
+# >>> ENZYMES(60)

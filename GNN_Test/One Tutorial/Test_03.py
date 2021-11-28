c1c680d0f3a3230804ffce5e3ec640cea37e6998
147 <lfw93814@gmail.com>
2021-11-28 16:22:58 +0800

    1

--- GNN_Test/One Tutorial/Test_03.py
+++ GNN_Test/One Tutorial/Test_03.py
@@ -2,10 +2,10 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
 df = pd.read_csv('../input/yoochoose-clicks.dat', header=None)
-df.columns=['session_id','timestamp','item_id','category']
+df.columns = ['session_id', 'timestamp', 'item_id', 'category']
 
 buy_df = pd.read_csv('../input/yoochoose-buys.dat', header=None)
-buy_df.columns=['session_id','timestamp','item_id','price','quantity']
+buy_df.columns = ['session_id', 'timestamp', 'item_id', 'price', 'quantity']
 
 item_encoder = LabelEncoder()
 df['item_id'] = item_encoder.fit_transform(df.item_id)
